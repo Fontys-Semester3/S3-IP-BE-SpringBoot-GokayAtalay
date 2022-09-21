c9afbd77c16d13351f85cef51487c4db1e4136f9
Gökay Atalay <485542@student.fontys.nl>
2022-09-21 11:17:37 +0200

    Created blank project

---
+++ S3-IP-BE-SpringBoot-GokayAtalay/src/main/java/com/example/Workethic/WorkethicApplication.java
@@ -0,0 +1,13 @@
+package com.example.Workethic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkethicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkethicApplication.class, args);
+	}
+
+}
---
+++ S3-IP-BE-SpringBoot-GokayAtalay/src/test/java/com/example/Workethic/WorkethicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Workethic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkethicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
