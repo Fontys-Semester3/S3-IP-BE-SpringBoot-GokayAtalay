dd74ec2477471962735835bd0c7c0cca7800f385
Jordy Walraven <jordywalraven1@gmail.com>
2022-12-19 15:49:50 +0100

    Adding create task

---
+++ S3-IP-BE-SpringBoot-GokayAtalay/src/main/java/com/example/Workethic/Repository/TaskPriorityRepository.java
@@ -0,0 +1,7 @@
+package com.example.Workethic.Repository;
+
+import com.example.Workethic.Models.TaskPriority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskPriorityRepository extends JpaRepository<TaskPriority,Long> {
+}
---
+++ S3-IP-BE-SpringBoot-GokayAtalay/src/main/java/com/example/Workethic/Services/Interfaces/ITaskPriorityService.java
@@ -0,0 +1,9 @@
+package com.example.Workethic.Services.Interfaces;
+
+import com.example.Workethic.Models.TaskPriority;
+
+import java.util.List;
+
+public interface ITaskPriorityService {
+    public List<TaskPriority> getAllPriorities();
+}
